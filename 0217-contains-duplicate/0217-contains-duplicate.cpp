108a1ce44685892f5cbf906b25e87168d3abdef3
trishankpaila29 <trishankpaila29@gmail.com>
2026-07-23 20:55:26 +0530

    Time: 31 ms (80.83%), Space: 82.6 MB (85.57%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        while(i<nums.size()-1) {
+            if(nums[i] ==nums[i+1]) {
+                return true;
+            }
+            else {
+                i++;
+            }
+        }
+        return false;
+    }
+};
